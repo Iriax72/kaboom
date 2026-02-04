35eb84c072ffa9ae750e97123c50c98e65554c12
flexult <touma.leandre.72@gmail.com>
2026-02-04 19:45:57 +0000

    meilleurs tests

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 import { Player } from "./game/Player.js";
 import { setUpControls } from "./game/controls.js";
 
-alert("loop: " + typeof loop);
-alert("vec2: " + typeof vec2);
-alert("keyIsDown" + typeof KeyIsDown);
-alert("unit" + typeof unit);
-
 kaboom({
     width: window.innerWidth,
     height: window.innerHeight,
     background: [10, 4, 35],
 });
+
+alert("loop: " + typeof loop + "\nvec2: " + typeof vec2 + "\nkeyIsDown: " + typeof keyIsDown + "\nunit: " + typeof unit);
 
 const player = new Player();
 setUpControls(player);
